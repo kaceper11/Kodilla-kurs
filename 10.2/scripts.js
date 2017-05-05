f6d651108210df3f59a75f099431c9b3d9ea06ad
kkepins1 <kfkepinski@gmail.com>
2017-05-05 19:34:12 +0200

    zadanie 10.2

---
+++ 10.2/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    var span = $('span');
+    span.each(function (index, element) {
+       if (span % 2 == 0) {
+           $(element).css('color', 'red');
+       }
+    });
+
+    var paragraphs = $('p');
+    paragraphs.each(function (index, element) {
+        var button = '<button class="btn" data-tmp="' + index + '">Kliknij</button>';
+        $(element).append(button);
+    })
+
+    $('button').click(function () {
+        alert($(this).attr('data-tmp'));
+    });
+});
