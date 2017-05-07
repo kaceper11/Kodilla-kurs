a47730a471eee9cc360eeb1dfefdef2e79d19283
kkepins1 <kfkepinski@gmail.com>
2017-05-07 12:36:16 +0200

    zadanie 10.3

--- 10.3/scripts.js
+++ 10.3/scripts.js
@@ -1,11 +1,29 @@
 $(document).ready(function () {
     var carouselList = $("#carousel ul");
-    setInterval(changeSlide, 3000);
-    carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+    setInterval(changeSlideNext, 6000);
+    function changeSlideNext() {
+        carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+    }
+    function changeSlidePrevious() {
+        carouselList.animate({'marginLeft': -400}, 500, moveLastSlide);
+    }
+    $('#next').click(function() {
+       changeSlideNext();
+    });
+    $('#previous').click(function() {
+        changeSlidePrevious();
+    });
+
     function moveFirstSlide() {
         var firstItem = carouselList.find("li:first");
         var lastItem = carouselList.find("li:last");
         lastItem.after(firstItem);
+        carouselList.css({marginLeft:0});
+    }
+    function moveLastSlide() {
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        firstItem.after(lastItem);
         carouselList.css({marginLeft:0});
     }
 });
